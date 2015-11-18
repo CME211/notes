046a850e7fcd2d3d33c60130ba98220f49efd255
Nick Henderson <nwh@stanford.edu>
2015-11-18 14:22:51 -0800

    add challenge

---
+++ lecture-23/challenge/max_drawdown.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+// note: this challenge is not complete
+
+int main() {
+
+  // load vector
+  
+  // reverse vector
+  
+  return 0;
+}
