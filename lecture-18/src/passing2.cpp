8085865f7cbfb0dd3f080dff9b58ae49b39eb005
asantucci <santucci.andreas@gmail.com>
2017-11-06 08:06:45 -0800

    Added necessary print statement.

--- lecture-18/src/passing2.cpp
+++ lecture-18/src/passing2.cpp
@@ -8,9 +8,9 @@ void increment(int a[2]) {
 int main() {
   int a[2] = {2, 3};
 
-  std::cout << "a[0] = " << ", " << "a[1] = " << std::endl;
+  std::cout << "a[0] = " << a[0] << ", " << "a[1] = " << a[1] << std::endl;
   increment(a);
-  std::cout << "a[0] = " << ", " << "a[1] = " << std::endl;
+  std::cout << "a[0] = " << a[0] << ", " << "a[1] = " << a[1] << std::endl;
   
   return 0;
 }
