4e4e34a50223f1373161ed9053970ed70d598fac
asantucci <santucci.andreas@gmail.com>
2018-11-04 10:08:49 -0800

    Now using a function, since lecture contents reorganized to introduce function definitions before this example.

--- lecture-12/src/simplifyPath.cpp
+++ lecture-12/src/simplifyPath.cpp
@@ -3,20 +3,21 @@
 #include <string>
 #include <vector>
 
+std::string simplifyPath(std::string path) {
+  std::string res, tmp;     
+  std::vector<std::string> stk;    
+  std::stringstream ss(path);     
+  while(getline(ss,tmp,'/')) {         
+    if (tmp == "" or tmp == ".")      continue;         
+    if (tmp == ".." and !stk.empty()) stk.pop_back();         
+    else if (tmp != "..")             stk.push_back(tmp);    
+  }     
+  for(auto str : stk) res += "/" + str;     
+  return res.empty() ? "/" : res; 
+}
+
+
 int main(int argc, char *argv[]) {
-    std::string res, tmp;
-    std::vector<std::string> stk;
     if (argc < 2) return 1;
-    std::stringstream ss(argv[1]);
-    while(getline(ss,tmp,'/')) {
-        if (tmp == "" or tmp == ".")
-            continue;
-        if (tmp == ".." and !stk.empty())
-            stk.pop_back();
-        else if (tmp != "..")
-            stk.push_back(tmp);
-    }
-    for(auto str : stk)
-        res += "/" + str;
-    std::cout << res << std::endl;
+    std::cout << simplifyPath(argv[1]) << std::endl;
 }
