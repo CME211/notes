85cbb8f7d0659295ec19afa809ab39ae1a7e1091
asantucci <santucci.andreas@gmail.com>
2018-11-11 12:39:02 -0800

    Changed print message to be hopefully more illustrative.

--- lecture-15/src/vector_memory.cpp
+++ lecture-15/src/vector_memory.cpp
@@ -3,14 +3,13 @@
 
 int main() {
   std::vector<int> a;
-  for (int i = 0; i < 10; i++) {
-    a.push_back(i);
-  }
-  std::cout << "sizeof(a): " << sizeof(a) << std::endl;
-  std::cout << "    memory location of a: " << &a << std::endl;
-  std::cout << " memory location of data: " << a.data() << std::endl;
-  std::cout << "difference in memory loc: "
-            << double((int*)&a-a.data()) / 1024 / 1024 / 1024 
-            << " GB" << std::endl;
+  for (int i = 0; i < 10; i++)
+      a.push_back(i);
+  std::cout << "sizeof(a)  : " << sizeof(a)   << std::endl;
+  std::cout << "sizeof(int): " << sizeof(int) << std::endl;
+  std::cout << "memory location of    a : " << &a << std::endl;
+  std::cout << "memory location of data : " << a.data() << std::endl;
+  std::cout << "memory location of a[0] : " << &a[0] << std::endl;  // Postfix operators bind tighter.
+  std::cout << "memory location of a[1] : " << &a[1] << std::endl;
   return 0;
 }
