4a7e16b25a633c6ba89492d8099877c176c53e0d
asantucci <santucci.andreas@gmail.com>
2018-11-11 12:38:41 -0800

    Made syntax more concise.

--- lecture-15/src/multi2.cpp
+++ lecture-15/src/multi2.cpp
@@ -7,16 +7,10 @@ int main() {
   a.resize(nrows*ncols);
 
   double n = 0.;
-  for(unsigned int i = 0; i < nrows; i++) {
-    for(unsigned int j = 0; j < ncols; j++) {
-      // manual indexing into "multi-dimensional array"
-      a[i*ncols + j] = n;
-      n++;
-    }
-  }
+  for(unsigned int i = 0; i < nrows; i++)
+    for(unsigned int j = 0; j < ncols; j++)
+      a[i*ncols + j] = n++; // manual indexing into "multi-dimensional array"
 
-  for(unsigned int i = 0; i < nrows*ncols; i++) {
+  for(unsigned int i = 0; i < nrows*ncols; i++)
     std::cout << "a[" << i << "] = " << a[i] << std::endl;
-  }
-  return 0;
 }
