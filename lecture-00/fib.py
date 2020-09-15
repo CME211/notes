475b202968df242d55e9b227c60c8e68ffc85757
Andreas <santucci@stanford.edu>
2020-09-14 19:45:13 -0700

    Added clarifying comments.

--- lecture-00/fib.py
+++ lecture-00/fib.py
@@ -1,14 +1,14 @@
 n = 10
 
 if n >= 0:
-    fn2 = 0
+    fn2 = 0             # Initialize f(n-2).
     print(fn2,end=',')
 if n >= 1:
-    fn1 = 1
+    fn1 = 1             # Initialize f(n-1).
     print(fn1,end=',')
-for i in range(2,n+1):
-    fn = fn1 + fn2
+for i in range(2,n+1):  # Here is where we iterate from 2:n.
+    fn = fn1 + fn2      # Recurrence relation is f(n) = f(n-1) + f(n-2).
     print(fn,end=',')
-    fn2 = fn1
-    fn1 = fn
+    fn2 = fn1           # Update our values...
+    fn1 = fn            # ...for both f(n-2) and f(n-1).
 print()
